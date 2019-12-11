71992ffaa81cdb2c7961ab9fd93833730247e423
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2019-12-11 14:20:19 +0100

    Update rainbow-web-sdk minimum version

--- SDKAudioVideoAngularSample/src/js/components/spectrum/spectrumCmp.js
+++ SDKAudioVideoAngularSample/src/js/components/spectrum/spectrumCmp.js
@@ -1,179 +1,183 @@
 angular.module("sample").component("rbxSpectrum", {
-    
-    bindings: {
-    },
-    
-    templateUrl: "./src/js/components/spectrum/spectrumCmp.template.html",
-    
-    controller : function rbcPhoneCtrl(rainbowSDK, $rootScope, $scope, Call) {
+  bindings: {},
 
-        "use strict";
+  templateUrl: "./src/js/components/spectrum/spectrumCmp.template.html",
 
-        var audioContext = new AudioContext();
-        var ctx = null;
-        var gradient = null;
-        var javascriptNode = null;
-        var remoteMediaStream = null;
-        var analyser = null;
-        var isSpectrumStarted = false;
+  controller: function rbcPhoneCtrl(rainbowSDK, $rootScope, $scope, Call) {
+    "use strict";
 
-        $scope.isInCommunication = false;
-        $scope.isSpectrumDisplayed = false;
+    var audioContext = new AudioContext();
+    var ctx = null;
+    var gradient = null;
+    var javascriptNode = null;
+    var remoteMediaStream = null;
+    var analyser = null;
+    var isSpectrumStarted = false;
 
-        this.$onInit = function() {
+    $scope.isInCommunication = false;
+    $scope.isSpectrumDisplayed = false;
 
-            // get the context from the canvas to draw on
-            ctx = $("#canvasSound").get()[0].getContext("2d");
+    this.$onInit = function() {
+      // get the context from the canvas to draw on
+      ctx = $("#canvasSound")
+        .get()[0]
+        .getContext("2d");
 
-            // create a gradient for the fill. Note the strange
-            // offset, since the gradient is calculated based on
-            // the canvas, not the specific element we draw
-            gradient = ctx.createLinearGradient(0, 0, 100, 0);
+      // create a gradient for the fill. Note the strange
+      // offset, since the gradient is calculated based on
+      // the canvas, not the specific element we draw
+      gradient = ctx.createLinearGradient(0, 0, 100, 0);
 
-            gradient.addColorStop(1, "red");
-            gradient.addColorStop(0.83, "orange");
-            gradient.addColorStop(0.66, "yellow");
-            gradient.addColorStop(0.50, "green");
-            gradient.addColorStop(0.33, "blue");
-            gradient.addColorStop(0.16, "indigo");
-            gradient.addColorStop(0, "violet");
+      gradient.addColorStop(1, "red");
+      gradient.addColorStop(0.83, "orange");
+      gradient.addColorStop(0.66, "yellow");
+      gradient.addColorStop(0.5, "green");
+      gradient.addColorStop(0.33, "blue");
+      gradient.addColorStop(0.16, "indigo");
+      gradient.addColorStop(0, "violet");
 
-            // Subscribe to contact change
-            document.addEventListener(rainbowSDK.webRTC.RAINBOW_ONWEBRTCCALLSTATECHANGED, onWebRTCCallChanged)));
+      // Subscribe to contact change
+      document.addEventListener(
+        rainbowSDK.webRTC.RAINBOW_ONWEBRTCCALLSTATECHANGED,
+        onWebRTCCallChanged
+      );
 
-            document.addEventListener(rainbowSDK.webRTC.RAINBOW_ONWEBRTCSTREAMADDED, onWebRTCStreamAdded)));
+      document.addEventListener(
+        rainbowSDK.webRTC.RAINBOW_ONWEBRTCSTREAMADDED,
+        onWebRTCStreamAdded
+      );
 
-            $rootScope.$on("$destroy", ($rootScope.$on("DEMO_ON_SPECTRUM_DISPLAY", onSpectrumDisplayChanged)));
+      $rootScope.$on(
+        "$destroy",
+        $rootScope.$on("DEMO_ON_SPECTRUM_DISPLAY", onSpectrumDisplayChanged)
+      );
+    };
 
-        };
+    this.$onDestroy = function() {};
 
-        this.$onDestroy = function() {
-        
-        };
+    var onWebRTCStreamAdded = function onWebRTCStreamAdded(event) {
+      var streams = event.detail;
+      console.log("[DEMO] :: >>> WebRTC stream added", streams);
+      if (!isSpectrumStarted) {
+        startSpectrum(streams);
+      }
+    };
 
-        var onWebRTCStreamAdded = function onWebRTCStreamAdded(event) {
-            var streams = event.detail;
-            console.log("[DEMO] :: >>> WebRTC stream added", streams);
-            if (!isSpectrumStarted) {
-                startSpectrum(streams);
-            }
-        };
+    var onSpectrumDisplayChanged = function onSpectrumDisplayChanged(
+      __event,
+      isDisplayed
+    ) {
+      $scope.isSpectrumDisplayed = isDisplayed;
+    };
 
-        var onSpectrumDisplayChanged = function onSpectrumDisplayChanged(__event, isDisplayed) {
-            $scope.isSpectrumDisplayed = isDisplayed;
-        };
-        
-        var onWebRTCCallChanged = function onWebRTCCallChanged(event) {
-            var call = event.detail;
+    var onWebRTCCallChanged = function onWebRTCCallChanged(event) {
+      var call = event.detail;
 
-            switch (call.status.value) {
-                
-                case Call.Status.RINGING_INCOMMING.value:
-                    break;
-                
-                case Call.Status.ACTIVE.value:
-                    $scope.isInCommunication = true;
-                    break;
-                
-                case Call.Status.UNKNOWN.value:
-                    $scope.isInCommunication = false;
-                    stopSpectrum();
-                    break;
-                
-                default:
-                    break;
-            }
-        };
+      switch (call.status.value) {
+        case Call.Status.RINGING_INCOMMING.value:
+          break;
 
-        var drawSpectrum = function drawSpectrum(array) {
-            for ( var i = 0; i < (array.length); i++ ) {
-                var value = array[i];
-                ctx.fillRect( 0, i * 3, value / 2.56, 2 );
-            }
-        };
+        case Call.Status.ACTIVE.value:
+          $scope.isInCommunication = true;
+          break;
 
-        var stopSpectrum = function stopSpectrum() {
-            isSpectrumStarted = false;
-            
-            if (javascriptNode) {
-                javascriptNode.disconnect();
-            }
+        case Call.Status.UNKNOWN.value:
+          $scope.isInCommunication = false;
+          stopSpectrum();
+          break;
 
-            if (remoteMediaStream) {
-                remoteMediaStream.disconnect();
-            }
-            
-            if (analyser) {
-                analyser.disconnect();
-            }
-        };
+        default:
+          break;
+      }
+    };
 
-        var startSpectrum = function manageSpectrum(streams) {
+    var drawSpectrum = function drawSpectrum(array) {
+      for (var i = 0; i < array.length; i++) {
+        var value = array[i];
+        ctx.fillRect(0, i * 3, value / 2.56, 2);
+      }
+    };
 
-            console.log("[DEMO] :: WebRTC manageSpectrum");
+    var stopSpectrum = function stopSpectrum() {
+      isSpectrumStarted = false;
 
-            var listOfRemoteStreams = streams;
+      if (javascriptNode) {
+        javascriptNode.disconnect();
+      }
 
-            var nWhichStream = 0;
+      if (remoteMediaStream) {
+        remoteMediaStream.disconnect();
+      }
 
-            // Error ?  Bad call ?
-            if ( listOfRemoteStreams.length > 0 ) {
+      if (analyser) {
+        analyser.disconnect();
+      }
+    };
 
-                console.log("[DEMO] :: WebRTC manageSpectrum streams", listOfRemoteStreams);
-                
-                // Which stream ?
-                if (listOfRemoteStreams.length > 1) {
-                    // TODO
-                    nWhichStream = 0;
-                }
+    var startSpectrum = function manageSpectrum(streams) {
+      console.log("[DEMO] :: WebRTC manageSpectrum");
 
-                // check for the stream
-                var audioTracks = listOfRemoteStreams[nWhichStream].getAudioTracks();
+      var listOfRemoteStreams = streams;
 
-                console.log("[DEMO] :: WebRTC manageSpectrum streams", audioTracks);
+      var nWhichStream = 0;
 
-                if ( audioTracks && audioTracks.length === 1 ) {
+      // Error ?  Bad call ?
+      if (listOfRemoteStreams.length > 0) {
+        console.log(
+          "[DEMO] :: WebRTC manageSpectrum streams",
+          listOfRemoteStreams
+        );
 
-                    // Get the remote stream and use it in the Audio stream
-                    remoteMediaStream = audioContext.createMediaStreamSource(listOfRemoteStreams[nWhichStream]);
-                    javascriptNode = audioContext.createScriptProcessor(1024, 1, 1);
-                    
-                    remoteMediaStream.connect(javascriptNode);
-                    
-                    // setup a analyzer
-                    analyser = audioContext.createAnalyser();
-                    analyser.smoothingTimeConstant = 0.3;
-                    analyser.fftSize = 512;
+        // Which stream ?
+        if (listOfRemoteStreams.length > 1) {
+          // TODO
+          nWhichStream = 0;
+        }
 
-                    // create a buffer source node
-                    remoteMediaStream.connect(analyser);
-                    analyser.connect(javascriptNode);
-                    javascriptNode.connect(audioContext.destination);
+        // check for the stream
+        var audioTracks = listOfRemoteStreams[nWhichStream].getAudioTracks();
 
-                    isSpectrumStarted = true;
+        console.log("[DEMO] :: WebRTC manageSpectrum streams", audioTracks);
 
-                    javascriptNode.onaudioprocess = function(__event){
-                        
-                        // get the average for the first channel
-                        var array =  new Uint8Array(analyser.frequencyBinCount);
-                        analyser.getByteFrequencyData(array);
+        if (audioTracks && audioTracks.length === 1) {
+          // Get the remote stream and use it in the Audio stream
+          remoteMediaStream = audioContext.createMediaStreamSource(
+            listOfRemoteStreams[nWhichStream]
+          );
+          javascriptNode = audioContext.createScriptProcessor(1024, 1, 1);
 
-                        // clear the current state
-                        //ctx.clearRect(0, 0, 500, 100);
-                        ctx.clearRect(0, 0, 100, 500);
-                        //ctx.fillRect(0, 0, 500, 20);
+          remoteMediaStream.connect(javascriptNode);
 
-                        // set the fill style
-                        ctx.fillStyle = gradient;
-                        drawSpectrum(array);
-                    };
+          // setup a analyzer
+          analyser = audioContext.createAnalyser();
+          analyser.smoothingTimeConstant = 0.3;
+          analyser.fftSize = 512;
 
-                }
-            }
-            else {
-                console.log("[DEMO] :: WebRTC manageSpectrum no stream found");
-            }
-        };
-    }
+          // create a buffer source node
+          remoteMediaStream.connect(analyser);
+          analyser.connect(javascriptNode);
+          javascriptNode.connect(audioContext.destination);
 
+          isSpectrumStarted = true;
+
+          javascriptNode.onaudioprocess = function(__event) {
+            // get the average for the first channel
+            var array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+
+            // clear the current state
+            //ctx.clearRect(0, 0, 500, 100);
+            ctx.clearRect(0, 0, 100, 500);
+            //ctx.fillRect(0, 0, 500, 20);
+
+            // set the fill style
+            ctx.fillStyle = gradient;
+            drawSpectrum(array);
+          };
+        }
+      } else {
+        console.log("[DEMO] :: WebRTC manageSpectrum no stream found");
+      }
+    };
+  }
 });
